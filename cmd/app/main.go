945401ceb6df0c2b8bcca584af2f17fb3acd264a
Vladyslav <ladislavkosenko@gmail.com>
2026-04-21 00:38:26 +0300

    feat: add zerolog, viper and GitHub Actions CI

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"os"
 
 	"github.com/ladislavkosenko/lab1-tooling/internal/calc"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	fmt.Println("Lab 1: Tooling setup is working!")
+	// 1. Налаштування красивого виводу логів для терміналу
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	result, err := calc.Divide(10.0, 2.0)
-	if err != nil {
-		log.Fatalf("Помилка ділення: %v", err)
+	// 2. Ініціалізація Viper для читання конфігурації
+	viper.SetConfigName("config") // ім'я файлу без розширення
+	viper.SetConfigType("yaml")   // тип файлу
+	viper.AddConfigPath(".")      // шукати файл у поточній директорії
+
+	// Спроба прочитати файл
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal().Err(err).Msg("Помилка читання файлу конфігурації")
 	}
 
-	fmt.Printf("Результат ділення: %f\n", result)
+	// Читаємо значення з конфігурації
+	appName := viper.GetString("app_name")
+	port := viper.GetInt("port")
+
+	// Виводимо лог інформаційного рівня
+	log.Info().Str("app", appName).Int("port", port).Msg("Програма успішно запущена!")
+
+	// Використовуємо наш калькулятор з першої лаби
+	result, err := calc.Divide(10.0, 2.0)
+	if err != nil {
+		log.Error().Err(err).Msg("Помилка під час ділення")
+		return
+	}
+
+	log.Info().Float64("result", result).Msg("Успішне обчислення")
 }
